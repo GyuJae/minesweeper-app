21146ee3e104f1a76a2ff052a4e0acbe03778dec
정규재 <wjdrbwo1206@naver.com>
2024-09-06 15:33:23 +0900

    refactor: fix DefaultBoard.of return type

--- app/(minesweeper)/models/board/default-board.ts
+++ app/(minesweeper)/models/board/default-board.ts
@@ -10,7 +10,7 @@ export class DefaultBoard extends Board {
     super();
   }
 
-  static of(gameLevel: GameLevel, cells: CellCollection): Board {
+  static of(gameLevel: GameLevel, cells: CellCollection): DefaultBoard {
     return new DefaultBoard(gameLevel, cells);
   }
 
