9ff773d069659cdf0f90687b6462c6a50b64283d
GyuJae <wjdrbwo1206@naver.com>
2024-09-08 16:35:05 +0900

    fix: 테스트 경로 수정

--- app/(minesweeper)/models/__tests__/minesweeper.test.ts
+++ app/(minesweeper)/models/__tests__/minesweeper.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test } from '@/libs/test';
 
 import { DefaultBoard } from '../board/default-board';
 import { GridCell } from '../cell/grid-cell';
