b325338cfd1ef5d33d83abfc0b897c8805620038
GyuJae <wjdrbwo1206@naver.com>
2024-09-08 16:34:22 +0900

    chore: test 설정

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,21 @@
-import react from "@vitejs/plugin-react";
-import { configDefaults, defineConfig } from "vitest/config";
+/* eslint-disable unicorn/import-style */
+import { resolve } from 'node:path';
+
+import react from '@vitejs/plugin-react';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom",
-    exclude: [...configDefaults.exclude, "e2e/**/*"],
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**/*'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(import.meta.dirname, './'),
+      },
+    ],
   },
 });
