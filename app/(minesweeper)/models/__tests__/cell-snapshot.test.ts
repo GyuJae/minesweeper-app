93cd9f9431bc8b3d0b7007e01f4054adf6790096
GyuJae <wjdrbwo1206@naver.com>
2024-09-14 14:58:32 +0900

    test: 셸 표시

---
+++ app/(minesweeper)/models/__tests__/cell-snapshot.test.ts
@@ -0,0 +1,57 @@
+import { describe, expect, test } from 'vitest';
+
+import { GridCell } from '../cell/grid-cell';
+import { GridCellPosition } from '../cell-position/grid-cell-position';
+import { ClosedCellSnapshot } from '../cell-snapshot/closed-cell-snapshot';
+import { OpendEmptyCellSnapshot } from '../cell-snapshot/opend-empty-cell-snapshot';
+import { OpendMineCellSnapshot } from '../cell-snapshot/opend-mine-cell-snapshot';
+import { OpendNumberCellSnapshot } from '../cell-snapshot/opend-number-cell-snapshot';
+import { CellState } from '../cell-state/cell-state.enum';
+import { MineCellType } from '../cell-type/mine-cell-type';
+import { NumberCellType } from '../cell-type/number-cell-type';
+
+describe('Cell - CellSnapshot', () => {
+  test('닫힌 셀은 닫힌 상태로 표시된다.', () => {
+    // given
+    const cell = GridCell.of(CellState.CLOSED, MineCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    const snapshot = cell.getSnapshot();
+
+    // then
+    expect(snapshot).toBeInstanceOf(ClosedCellSnapshot);
+  });
+
+  test('지뢰가 있는 열린 셀은 지뢰를 표시한다.', () => {
+    // given
+    const cell = GridCell.of(CellState.OPENED, MineCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    const snapshot = cell.getSnapshot();
+
+    // then
+    expect(snapshot).toBeInstanceOf(OpendMineCellSnapshot);
+  });
+
+  test('열린 셀은 주변 지뢰 개수를 표시한다.', () => {
+    // given
+    const cell = GridCell.of(CellState.OPENED, NumberCellType.of(1), GridCellPosition.of(0, 0));
+
+    // when
+    const snapshot = cell.getSnapshot();
+
+    // then
+    expect(snapshot).toBeInstanceOf(OpendNumberCellSnapshot);
+  });
+
+  test('주변에 지뢰가 없는 열린 셀은 비어 있는 상태로 표시된다.', () => {
+    // given
+    const cell = GridCell.of(CellState.OPENED, NumberCellType.of(0), GridCellPosition.of(0, 0));
+
+    // when
+    const snapshot = cell.getSnapshot();
+
+    // then
+    expect(snapshot).toBeInstanceOf(OpendEmptyCellSnapshot);
+  });
+});
