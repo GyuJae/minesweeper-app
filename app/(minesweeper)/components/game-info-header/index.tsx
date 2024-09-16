a2a743766f2aa8002bdc442ed4a9e5837e3edb5c
GyuJae <wjdrbwo1206@naver.com>
2024-09-16 16:44:20 +0900

    feat: 다시시작하기, 일시 정지하기

--- app/(minesweeper)/components/game-info-header/index.tsx
+++ app/(minesweeper)/components/game-info-header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from '@/components/ui/context-menu';
+
 import { useMinesweeperBoard } from '../../context/minesweeper-board.provider';
 import { useMinesweeperGameConfig } from '../../context/minesweeper-game-config.provider';
 import { GameLevel } from '../../models/game-level/game-level.enum';
@@ -10,30 +12,51 @@ const GameInfoHeader = () => {
   const gameConfigContext = useMinesweeperGameConfig();
   const boardContext = useMinesweeperBoard();
 
-  const onChangeGameLevel = (selectedLevel: GameLevel) => {
+  const onChangeLevel = (selectedLevel: GameLevel) => {
     gameConfigContext.setGameLevel(selectedLevel);
     boardContext.resetByGameLevel(selectedLevel);
     gameConfigContext.setGameStatus(GameStatus.READY);
   };
 
-  // const onResetBoard: MouseEventHandler<HTMLButtonElement> = () => {
-  //   boardContext.resetByGameLevel(gameConfigContext.gameLevel);
-  //   gameConfigContext.setGameStatus(GameStatus.READY);
-  // };
+  const onRestart = () => {
+    boardContext.resetByGameLevel(gameConfigContext.gameLevel);
+    gameConfigContext.setGameStatus(GameStatus.READY);
+  };
+
+  const onPaused = () => {
+    gameConfigContext.setGameStatus(GameStatus.PAUSED);
+  };
 
   return (
     <header className='flex w-full items-center justify-between py-4'>
       <nav>
         <GameLevelSelect
           options={GameLevel.findAllLevels()}
-          onChangeLevel={onChangeGameLevel}
+          onChangeLevel={onChangeLevel}
           selectedOption={gameConfigContext.gameLevel}
         />
       </nav>
+
       <section>
-        <p className='text-5xl'>{gameConfigContext.gameStatus.getEmoji()}</p>
+        <ContextMenu>
+          <ContextMenuTrigger>
+            <p className='text-5xl'>{gameConfigContext.gameStatus.getEmoji()}</p>
+          </ContextMenuTrigger>
+          <ContextMenuContent className='w-64'>
+            <ContextMenuItem inset onClick={onRestart}>
+              처음부터
+            </ContextMenuItem>
+            <ContextMenuItem
+              inset
+              disabled={!GameStatus.PLAYING.equals(gameConfigContext.gameStatus)}
+              onClick={onPaused}
+            >
+              일시정지
+            </ContextMenuItem>
+          </ContextMenuContent>
+        </ContextMenu>
       </section>
-      <section className='flex gap-4 text-lg'>
+      <section className='grid grid-cols-2 gap-4 text-lg'>
         <p>🕰️ {gameConfigContext.overSeconds}</p>
         <p>🚩 {boardContext.board.getRemainingFlagCount()}</p>
       </section>
