6523b3ec6631d4e6107c2ae4c572295ad89ff26c
GyuJae <wjdrbwo1206@naver.com>
2024-09-16 11:23:33 +0900

    test: 셸의 상태에 따라 열 수 있는 조건이 다릅니다.

--- app/(minesweeper)/models/__tests__/cell-snapshot.test.ts
+++ app/(minesweeper)/models/__tests__/cell-snapshot.test.ts
@@ -17,10 +17,8 @@ describe('Cell - CellSnapshot', () => {
     const cell = GridCell.of(CellState.CLOSED, MineCellType.of(), GridCellPosition.of(0, 0));
 
     // when
-    const snapshot = cell.getSnapshot();
-
     // then
-    expect(snapshot).toBeInstanceOf(ClosedCellSnapshot);
+    expect(cell.getSnapshot()).toBeInstanceOf(ClosedCellSnapshot);
   });
 
   test('지뢰가 있는 열린 셀은 지뢰를 표시한다.', () => {
@@ -28,10 +26,8 @@ describe('Cell - CellSnapshot', () => {
     const cell = GridCell.of(CellState.OPENED, MineCellType.of(), GridCellPosition.of(0, 0));
 
     // when
-    const snapshot = cell.getSnapshot();
-
     // then
-    expect(snapshot).toBeInstanceOf(OpenedMineCellSnapshot);
+    expect(cell.getSnapshot()).toBeInstanceOf(OpenedMineCellSnapshot);
   });
 
   test('열린 셀은 주변 지뢰 개수를 표시한다.', () => {
@@ -39,10 +35,8 @@ describe('Cell - CellSnapshot', () => {
     const cell = GridCell.of(CellState.OPENED, NumberCellType.of(1), GridCellPosition.of(0, 0));
 
     // when
-    const snapshot = cell.getSnapshot();
-
     // then
-    expect(snapshot).toBeInstanceOf(OpenedNumberCellSnapshot);
+    expect(cell.getSnapshot()).toBeInstanceOf(OpenedNumberCellSnapshot);
   });
 
   test('주변에 지뢰가 없는 열린 셀은 비어 있는 상태로 표시된다.', () => {
@@ -50,9 +44,43 @@ describe('Cell - CellSnapshot', () => {
     const cell = GridCell.of(CellState.OPENED, EmptyCellType.of(), GridCellPosition.of(0, 0));
 
     // when
-    const snapshot = cell.getSnapshot();
-
     // then
-    expect(snapshot).toBeInstanceOf(OpenedEmptyCellSnapshot);
+    expect(cell.getSnapshot()).toBeInstanceOf(OpenedEmptyCellSnapshot);
+  });
+
+  test('열린 셸은 다시 열 수 없습니다.', () => {
+    // given
+    const cell = GridCell.of(CellState.OPENED, EmptyCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    // then
+    expect(cell.isDisabledOpenCell()).toBeTruthy();
+  });
+
+  test('깃발이 꽂힌 셀은 열 수 없습니다.', () => {
+    // given
+    const cell = GridCell.of(CellState.FLAGGED, EmptyCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    // then
+    expect(cell.isDisabledOpenCell()).toBeTruthy();
+  });
+
+  test('닫힌 셸은 열 수 있습니다.', () => {
+    // given
+    const cell = GridCell.of(CellState.CLOSED, EmptyCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    // then
+    expect(cell.isDisabledOpenCell()).toBeFalsy();
+  });
+
+  test('지뢰가 있는 열린 셸은 열 수 없습니다.', () => {
+    // given
+    const cell = GridCell.of(CellState.OPENED, MineCellType.of(), GridCellPosition.of(0, 0));
+
+    // when
+    // then
+    expect(cell.isDisabledOpenCell()).toBeTruthy();
   });
 });
