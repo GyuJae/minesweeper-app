64969c566668ebb6f649f88761ba8b76548ce92b
GyuJae <wjdrbwo1206@naver.com>
2024-09-16 13:38:08 +0900

    test: 처음 모두 닫혀 있는 보드에 깃발을 꽂은 후, 셸 하나 열였을 때 모든 깃발은 제거가 됩니다.

--- app/(minesweeper)/models/__tests__/minesweeper.test.ts
+++ app/(minesweeper)/models/__tests__/minesweeper.test.ts
@@ -851,4 +851,18 @@ describe('지뢰찾기 게임 규칙', () => {
     // then
     expect(mockCallback).not.toHaveBeenCalled();
   });
+
+  test('처음 모두 닫혀 있는 보드에 깃발을 꽂은 후, 셸 하나 열였을 때 모든 깃발은 제거가 됩니다.', () => {
+    // given
+    const board = DefaultBoard.of(GameLevel.VERY_EASY);
+    const newBoard = board.toggleFlag(GridCellPosition.of(0, 0));
+
+    // when
+    const openedBoard = newBoard.openCell(GridCellPosition.of(0, 1));
+
+    // then
+    expect(openedBoard.getUnOpenedMineCount()).toBe(10);
+    expect(board.findCellByPosition(GridCellPosition.of(0, 1)).isOpened()).toBeTruthy();
+    expect(openedBoard.findCellByPosition(GridCellPosition.of(0, 0)).isFlagged()).toBeFalsy();
+  });
 });
