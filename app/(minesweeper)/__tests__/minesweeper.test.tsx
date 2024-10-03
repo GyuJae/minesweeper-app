dd255473c404b8fd826adfaf346a06bd1555f571
GyuJae <wjdrbwo1206@naver.com>
2024-10-03 19:31:03 +0900

    test: add

--- app/(minesweeper)/__tests__/minesweeper.test.tsx
+++ app/(minesweeper)/__tests__/minesweeper.test.tsx
@@ -1,4 +1,4 @@
-import { beforeAll, describe, expect, render, screen, test, userEvent } from '@/libs/test';
+import { afterEach, beforeAll, cleanup, describe, expect, render, screen, test, userEvent, vi } from '@/libs/test';
 
 import { MinesweeperBoardProvider } from '../context/minesweeper-board.provider';
 import { MinesweeperGameConfigProvider } from '../context/minesweeper-game-config.provider';
@@ -15,6 +15,7 @@ import { GameLevel } from '../models/game-level/game-level.enum';
 import Minesweeper from '../page';
 
 const testHelper = {
+  EMPTY_CELL_TEXT: '',
   clickCell: async (position: CellPosition) => {
     await userEvent.click(
       screen.getByRole('button', {
@@ -22,67 +23,280 @@ const testHelper = {
       }),
     );
   },
+  findCell: (position: CellPosition) => {
+    return screen.getByRole('button', {
+      name: `cell-${position.toString()}`,
+    });
+  },
 };
 
-beforeAll(() => {});
-
 describe('<Minesweeper />', () => {
-  test('사용자가 지뢰가 아닌 모든 셀을 열면 게임에서 승리합니다. 승리 메시지가 출력됩니다.', async () => {
-    // given
-    const gameLevel = GameLevel.VERY_EASY;
-    const defaultBoard = DefaultBoard.of(
-      gameLevel,
-      GridCellCollection.of(gameLevel, [
-        [
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(0, 0)),
-          GridCell.of(CellState.CLOSED, MineCellType.of(), GridCellPosition.of(0, 1)),
-          GridCell.of(CellState.CLOSED, MineCellType.of(), GridCellPosition.of(0, 2)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(0, 3)),
-        ],
-        [
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(1, 0)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(2), GridCellPosition.of(1, 1)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(2), GridCellPosition.of(1, 2)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(1, 3)),
-        ],
-        [
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(2, 0)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(2, 1)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(2, 2)),
-          GridCell.of(CellState.CLOSED, EmptyCellType.of(), GridCellPosition.of(2, 3)),
-        ],
-        [
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(3, 0)),
-          GridCell.of(CellState.CLOSED, MineCellType.of(), GridCellPosition.of(3, 1)),
-          GridCell.of(CellState.CLOSED, NumberCellType.of(1), GridCellPosition.of(3, 2)),
-          GridCell.of(CellState.CLOSED, EmptyCellType.of(), GridCellPosition.of(3, 3)),
-        ],
-      ]),
-    );
-
-    render(
-      <MinesweeperGameConfigProvider gameLevel={gameLevel}>
-        <MinesweeperBoardProvider gameLevel={gameLevel} defaultBoard={defaultBoard}>
-          <Minesweeper />
-        </MinesweeperBoardProvider>
-      </MinesweeperGameConfigProvider>,
-    );
-
-    await testHelper.clickCell(GridCellPosition.of(0, 0));
-    await testHelper.clickCell(GridCellPosition.of(0, 3));
-    await testHelper.clickCell(GridCellPosition.of(1, 0));
-    await testHelper.clickCell(GridCellPosition.of(1, 1));
-    await testHelper.clickCell(GridCellPosition.of(1, 2));
-    await testHelper.clickCell(GridCellPosition.of(1, 3));
-    await testHelper.clickCell(GridCellPosition.of(2, 0));
-    await testHelper.clickCell(GridCellPosition.of(2, 1));
-    await testHelper.clickCell(GridCellPosition.of(2, 2));
-    await testHelper.clickCell(GridCellPosition.of(2, 3));
-    await testHelper.clickCell(GridCellPosition.of(3, 0));
-    await testHelper.clickCell(GridCellPosition.of(3, 2));
-    await testHelper.clickCell(GridCellPosition.of(3, 3));
-
-    // then
-    expect(screen.getByText('🥳')).toBeDefined();
+  beforeAll(() => {
+    Object.defineProperty(global.HTMLMediaElement.prototype, 'play', {
+      configurable: true,
+      writable: true,
+      value: vi.fn().mockResolvedValue(null),
+    });
+    Object.defineProperty(global.HTMLCanvasElement.prototype, 'getContext', {
+      configurable: true,
+      writable: true,
+      value: vi.fn().mockResolvedValue(null),
+    });
   });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test.each([{ cellCollection: GridCellCollection, cell: GridCell, cellPosition: GridCellPosition }])(
+    '사용자가 지뢰가 아닌 모든 셀을 열면 게임에서 승리합니다. 승리 메시지가 출력됩니다.',
+    async ({ cell, cellPosition, cellCollection }) => {
+      // given
+      const gameLevel = GameLevel.VERY_EASY;
+      const defaultBoard = DefaultBoard.of(
+        gameLevel,
+        cellCollection.of(gameLevel, [
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 1)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(2, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(3, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(3, 3)),
+          ],
+        ]),
+      );
+
+      render(
+        <MinesweeperGameConfigProvider gameLevel={gameLevel}>
+          <MinesweeperBoardProvider gameLevel={gameLevel} defaultBoard={defaultBoard}>
+            <Minesweeper />
+          </MinesweeperBoardProvider>
+        </MinesweeperGameConfigProvider>,
+      );
+
+      // when
+      await testHelper.clickCell(cellPosition.of(0, 0));
+      await testHelper.clickCell(cellPosition.of(0, 3));
+      await testHelper.clickCell(cellPosition.of(1, 0));
+      await testHelper.clickCell(cellPosition.of(1, 1));
+      await testHelper.clickCell(cellPosition.of(1, 2));
+      await testHelper.clickCell(cellPosition.of(1, 3));
+      await testHelper.clickCell(cellPosition.of(2, 0));
+      await testHelper.clickCell(cellPosition.of(2, 1));
+      await testHelper.clickCell(cellPosition.of(2, 2));
+      await testHelper.clickCell(cellPosition.of(2, 3));
+      await testHelper.clickCell(cellPosition.of(3, 0));
+      await testHelper.clickCell(cellPosition.of(3, 2));
+      await testHelper.clickCell(cellPosition.of(3, 3));
+
+      // then
+      expect(screen.getByText('🥳')).toBeDefined();
+    },
+  );
+
+  test.each([{ cellCollection: GridCellCollection, cell: GridCell, cellPosition: GridCellPosition }])(
+    '숫자 셸을 클릭하면 숫자가 표시된다.',
+    async ({ cell, cellPosition, cellCollection }) => {
+      // given
+      const gameLevel = GameLevel.VERY_EASY;
+      const defaultBoard = DefaultBoard.of(
+        gameLevel,
+        cellCollection.of(gameLevel, [
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 1)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(2, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(3, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(3, 3)),
+          ],
+        ]),
+      );
+
+      render(
+        <MinesweeperGameConfigProvider gameLevel={gameLevel}>
+          <MinesweeperBoardProvider gameLevel={gameLevel} defaultBoard={defaultBoard}>
+            <Minesweeper />
+          </MinesweeperBoardProvider>
+        </MinesweeperGameConfigProvider>,
+      );
+
+      // when
+      await testHelper.clickCell(cellPosition.of(0, 0));
+
+      // then
+      expect(testHelper.findCell(cellPosition.of(0, 0)).textContent).toBe('1');
+    },
+  );
+
+  test.each([{ cellCollection: GridCellCollection, cell: GridCell, cellPosition: GridCellPosition }])(
+    '빈 공간을 클릭하면 주변 숫자 셀이 모두 열린다.',
+    async ({ cell, cellPosition, cellCollection }) => {
+      // given
+      const gameLevel = GameLevel.VERY_EASY;
+      const defaultBoard = DefaultBoard.of(
+        gameLevel,
+        cellCollection.of(gameLevel, [
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 1)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(2, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(3, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(3, 3)),
+          ],
+        ]),
+      );
+
+      render(
+        <MinesweeperGameConfigProvider gameLevel={gameLevel}>
+          <MinesweeperBoardProvider gameLevel={gameLevel} defaultBoard={defaultBoard}>
+            <Minesweeper />
+          </MinesweeperBoardProvider>
+        </MinesweeperGameConfigProvider>,
+      );
+
+      // when
+      await testHelper.clickCell(cellPosition.of(2, 3));
+
+      // then
+      expect(testHelper.findCell(cellPosition.of(0, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(0, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(0, 2)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(0, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+
+      expect(testHelper.findCell(cellPosition.of(1, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(1, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(1, 2)).textContent).toBe('2');
+      expect(testHelper.findCell(cellPosition.of(1, 3)).textContent).toBe('1');
+
+      expect(testHelper.findCell(cellPosition.of(2, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(2, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(2, 2)).textContent).toBe('1');
+      expect(testHelper.findCell(cellPosition.of(2, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+
+      expect(testHelper.findCell(cellPosition.of(3, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(3, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(3, 2)).textContent).toBe('1');
+      expect(testHelper.findCell(cellPosition.of(3, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+    },
+  );
+
+  test.each([{ cellCollection: GridCellCollection, cell: GridCell, cellPosition: GridCellPosition }])(
+    '지뢰셸을 클릭하면 모든 지뢰 셸이 열리면서 게임이 종료된다.',
+    async ({ cell, cellPosition, cellCollection }) => {
+      // given
+      const gameLevel = GameLevel.VERY_EASY;
+      const defaultBoard = DefaultBoard.of(
+        gameLevel,
+        cellCollection.of(gameLevel, [
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 1)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(0, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(0, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(2), cellPosition.of(1, 2)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(1, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 0)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(2, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(2, 3)),
+          ],
+          [
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 0)),
+            cell.of(CellState.CLOSED, MineCellType.of(), cellPosition.of(3, 1)),
+            cell.of(CellState.CLOSED, NumberCellType.of(1), cellPosition.of(3, 2)),
+            cell.of(CellState.CLOSED, EmptyCellType.of(), cellPosition.of(3, 3)),
+          ],
+        ]),
+      );
+
+      render(
+        <MinesweeperGameConfigProvider gameLevel={gameLevel}>
+          <MinesweeperBoardProvider gameLevel={gameLevel} defaultBoard={defaultBoard}>
+            <Minesweeper />
+          </MinesweeperBoardProvider>
+        </MinesweeperGameConfigProvider>,
+      );
+
+      // when
+      await testHelper.clickCell(cellPosition.of(0, 1));
+
+      // then
+      expect(testHelper.findCell(cellPosition.of(0, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(0, 1)).textContent).toBe('💣');
+      expect(testHelper.findCell(cellPosition.of(0, 2)).textContent).toBe('💣');
+      expect(testHelper.findCell(cellPosition.of(0, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+
+      expect(testHelper.findCell(cellPosition.of(1, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(1, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(1, 2)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(1, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+
+      expect(testHelper.findCell(cellPosition.of(2, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(2, 1)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(2, 2)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(2, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+
+      expect(testHelper.findCell(cellPosition.of(3, 0)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(3, 1)).textContent).toBe('💣');
+      expect(testHelper.findCell(cellPosition.of(3, 2)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+      expect(testHelper.findCell(cellPosition.of(3, 3)).textContent).toBe(testHelper.EMPTY_CELL_TEXT);
+    },
+  );
 });
