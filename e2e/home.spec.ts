cc25aaf0b2ba4ca6312b526fddd60a500fc2e2eb
GyuJae <wjdrbwo1206@naver.com>
2024-10-06 14:43:12 +0900

    deelte: playwright

--- e2e/home.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("loads the home page", async ({ page }) => {
-  await page.goto("/");
-
-  await expect(page.locator("h1")).toContainText("HOME");
-});
